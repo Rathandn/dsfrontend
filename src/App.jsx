67c7f58ec8e26ca99408a818b88434a23d3d6dfa
NoobStack_Rathan <rathandivate27@gmail.com>
2025-10-26 13:55:50 +0530

    404 not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Products from './pages/Products'
@@ -8,10 +8,12 @@ import AdminLogin from './pages/AdminLogin'
 import Admin from './pages/Admin'
 import UploadProduct from './pages/UploadProduct'
 import AdminCategories from './pages/AdminCategories'
+// import { div } from 'framer-motion/client'
 
 const App = () => {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    // <div className='bg-black text-3xl text-blue-500'><P>Hello how are u</P></div>
+    <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar />
         <Routes>
@@ -23,10 +25,10 @@ const App = () => {
           <Route path="/admin-login" element={<AdminLogin />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/admin/upload" element={<UploadProduct />} />
-          <Route path="/admin/categories" element={<AdminCategories />} />
+           <Route path="/admin/categories" element={<AdminCategories />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
 
